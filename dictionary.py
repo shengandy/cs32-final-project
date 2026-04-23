4a840a81a53ce96232b52f8accff0f74b1b34a65
Andy Sheng <andysheng@college.harvard.edu>
2026-04-22 23:21:45 -0400

    Uploading from earlier files (wrong file folder)

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+WEAK_WORDS = {
+    "very",
+    "really",
+    "good",
+    "bad",
+    "nice",
+    "thing",
+    "stuff",
+    "big",
+    "small",
+}
+
+THESAURUS = {
+    "very": ["extremely", "highly", "greatly"],
+    "really": ["truly", "genuinely", "actually"],
+    "good": ["strong", "effective", "excellent"],
+    "bad": ["poor", "weak", "harmful"],
+    "nice": ["pleasant", "appealing", "thoughtful"],
+    "big": ["large", "major", "significant"],
+    "small": ["tiny", "minor", "compact"],
+    "thing": ["item", "idea", "element"],
+    "stuff": ["materials", "items", "content"],
+}
